a586d09bdff01bcb76ac473282dc03864586c801
xiaobai <xiaobai@asyou.me>
2016-01-20 07:35:54 +0800

    update task file

--- pkg/pulic_type/conf.go
+++ pkg/pulic_type/conf.go
@@ -1,14 +1,16 @@
 package pulic_type
 
-type ConfType struct {
-	Name     string                  `json:"name"`
-	MicroSer map[string]MicroSerType `json:"micro_ser"`
-	Version  string                  `json:"version"`
-}
+type (
+	ConfType struct {
+		Name     string                  `json:"name"`
+		MicroSer map[string]MicroSerType `json:"micro_ser"`
+		Version  string                  `json:"version"`
+	}
 
-type MicroSerType struct {
-	Addr   string            `json:"addr"`
-	AppId  string            `json:"app_id"`
-	Appkey string            `json:"app_key"`
-	Attr   map[string]string `json:"attr"`
-}
+	MicroSerType struct {
+		Addr   string            `json:"addr"`
+		AppId  string            `json:"app_id"`
+		Appkey string            `json:"app_key"`
+		Attr   map[string]string `json:"attr"`
+	}
+)
